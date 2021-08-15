a07ba93aa470a2ed1e836d84ae352f8062b48c22
Pedro <phaquino1998@gmail.com>
2021-08-14 22:06:49 -0300

    * add multitasking support to thread.cc * expanded implementation of Task * add multitasking to thread_init

--- src/api/thread.cc
+++ src/api/thread.cc
@@ -38,6 +38,9 @@ void Thread::constructor_epilogue(Log_Addr entry, unsigned int stack_size)
 
     assert((_state != WAITING) && (_state != FINISHING)); // invalid states
 
+    if(multitask)
+        _task->insert(this);
+
     if((_state != READY) && (_state != RUNNING))
         _scheduler.suspend(this);
 
@@ -85,6 +88,10 @@ Thread::~Thread()
         break;
     }
 
+    if(multitask) {
+        _task->remove(this);
+        delete _user_stack;
+    }
     if(_joining)
         _joining->resume();
 
@@ -341,6 +348,9 @@ void Thread::dispatch(Thread * prev, Thread * next, bool charge)
             db<Thread>(INF) << "Thread::dispatch:prev={" << prev << ",ctx=" << tmp << "}" << endl;
         }
         db<Thread>(INF) << "Thread::dispatch:next={" << next << ",ctx=" << *next->_context << "}" << endl;
+
+        if(multitask && (next->_task != prev->_task))
+            next->_task->activate();
 
         // The non-volatile pointer to volatile pointer to a non-volatile context is correct
         // and necessary because of context switches, but here, we are locked() and
