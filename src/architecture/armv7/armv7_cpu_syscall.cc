4b5f28b524a5b6f3e551e24b4978eeda7367ecb6
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2021-08-30 21:56:36 -0300

    Upgrade syscalls, make test run

--- src/architecture/armv7/armv7_cpu_syscall.cc
+++ src/architecture/armv7/armv7_cpu_syscall.cc
@@ -7,8 +7,20 @@ __BEGIN_SYS
 
 void CPU::syscall(void * message)
 {
+    // TODO @cross: erase comment
+    // Isso é do guto
     CPU::r1(reinterpret_cast<CPU::Reg>(message));
     CPU::svc();
+
+    // // TODO @cross: isso eh nosso, se pah tem que guardar contexto aqui
+    // //                  ver se ta em outro lugar.
+    // //                   na real, parece que o syscalled ta cuidando disso sussa.
+    // ASM("push {lr}\n");
+
+    // CPU::r0(reinterpret_cast<CPU::Reg>(message));
+    // ASM("svc   0x0");
+
+    // ASM("pop {lr}\n");
 }
 
 __END_SYS
