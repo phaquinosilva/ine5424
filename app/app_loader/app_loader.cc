87cffe0d2f80c0217dbe4948983f5a67df00a4a6
Gabriel Simonetto <GabrielSimonetto@users.noreply.github.com>
2021-09-25 17:44:32 -0300

    Wip: having trouble with imports

--- app/app_loader/app_loader.cc
+++ app/app_loader/app_loader.cc
@@ -1,4 +1,8 @@
 #include <utility/ostream.h>
+#include <utility/elf.h>
+#include <architecture.h>
+#include <system.h>
+#include <time.h>
 #include <syscall/stub_fork.h>
 
 using namespace EPOS;
@@ -13,6 +17,39 @@ int main(int argc, char ** argv)
     cout << argc_2 << "\n";
     cout << "======= ESSE EH O ARGV DA MASSA =======" << "\n";
     cout << argv << "\n";
+
+    cout << "Loading beginned" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    // unsigned end = ((unsigned)&argv < Application::APP_DATA) ? Application::APP_DATA : (unsigned)MMU::align_page(&_end);
+    // unsigned end = ((unsigned)&argv < Application::APP_DATA) ? Application::APP_DATA : (unsigned)MMU::align_page(&argv);
+    // int * extras = static_cast<int*>(MMU::align_page(end) + Application::HEAP_SIZE);
+    
+    unsigned HEAP_SIZE = 16 * 1024;
+    unsigned end = (unsigned)MMU::align_page(&argv);
+    int * extras = static_cast<int*>(MMU::align_page(end) + HEAP_SIZE);
+    // cout << "end=" << end << endl;
+    // cout << "Extras is located at addr=" << extras << endl;
+
+        // TODO fazer o WHILE de varios app.
+    // for (int app_size = *extras; app_size; extras += app_size/4, app_size = *reinterpret_cast<int*>(extras)) {
+    // while
+
+    
+    ELF * app_elf = reinterpret_cast<ELF *>(++extras);
+    if (!app_elf->valid()) {
+        cout << "Skipping corrupted App" << endl;
+    } else {
+        cout << "This Elf was actually valid :D !" << endl;
+    }
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+
+
+
 
     // cout << "======= ESSE EH O ARGC DA MASSA =======" << "\n" << endl;
     // cout << argc_2 << "\n" << endl;
