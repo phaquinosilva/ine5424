a191a416676b0b2dd4652583eba3cd61cc25791a
Gabriel Simonetto <GabrielSimonetto@users.noreply.github.com>
2021-09-26 15:41:47 -0300

    Fix bugs

--- include/utility/load_app.h
+++ include/utility/load_app.h
@@ -38,45 +38,87 @@ int load_app(int off_set, unsigned int addr) {
         for(int i = 1; i < app_elf->segments(); i++) {
             if(app_elf->segment_type(i) != PT_LOAD)
                 continue;
-            if(app_elf->segment_address(i) < app_data)
-                app_data = app_elf->segment_address(i);
-            app_data_size = app_elf->segment_size(i);
+            // if(app_elf->segment_address(i) < app_data)
+            //     app_data = app_elf->segment_address(i);
+            app_data_size += app_elf->segment_size(i);
         }
     }
+
+    // TODO @cross this might be very wrong because app_data never changes
     if(app_data == ~0U) {
         db<Setup>(WRN) << "APP ELF image has no data segment!" << endl;
         app_data = MMU::align_page(Memory_Map::APP_DATA);
     }
     if(Traits<System>::multiheap) { // Application heap in data segment
         app_data_size = MMU::align_page(app_data_size);
-        app_data_size = MMU::align_page(Traits<Application>::STACK_SIZE);
+        app_data_size += MMU::align_page(Traits<Application>::STACK_SIZE);
+        app_data_size += MMU::align_page(Traits<Application>::HEAP_SIZE);
     }
 
-    if(app_elf->load_segment(0) < 0) {
+
+    typedef int (Main)(); // TODO @cross se pah ta faltando o argc e argv aqui
+    // typedef int (Main)(argc, argv); // TODO @cross se pah ta faltando o argc e argv aqui
+    Main * main = reinterpret_cast<Main *>(entry);
+
+    // argc = ...;
+    // argv = ...;
+
+
+    // Address_Space* as = new (SYSTEM) Address_Space(MMU::current());
+    Segment* cs = new (SYSTEM) Segment(Log_Addr(app_code), app_code_size, Segment::Flags::APPC);
+    Segment* ds = new (SYSTEM) Segment(Log_Addr(app_data), app_data_size, Segment::Flags::APPD);
+
+    // cout << "Attaching data segment..." << endl;
+    CPU::int_disable();
+    Log_Addr code = Task::self()->address_space()->attach(cs);
+    Log_Addr data = Task::self()->address_space()->attach(ds);
+    CPU::int_enable();
+
+    // app_code = app_elf->segment_address(0);
+
+    if(app_elf->load_segment(0, code) < 0) {
         db<Setup>(ERR) << "Application code segment was corrupted during SETUP!" << endl;
         Machine::panic();
     }
     for(int i = 1; i < app_elf->segments(); i++) {
-        if(app_elf->load_segment(i) < 0) {
+        if(app_elf->load_segment(i, data) < 0) {
             db<Setup>(ERR) << "Application data segment was corrupted during SETUP!" << endl;
             Machine::panic();
         }
+        data += app_elf->segment_size(i);
     }
 
-    typedef int (Main)();
-    Main * main = reinterpret_cast<Main *>(entry);
-    Address_Space* as = new (SYSTEM) Address_Space(MMU::current());
-    Segment* cs = new (SYSTEM) Segment(Log_Addr(app_code), app_code_size, Segment::Flags::APPC);
-    Segment* ds = new (SYSTEM) Segment(Log_Addr(app_data), app_data_size, Segment::Flags::APPD);
-    Log_Addr code = app_code;
-    Log_Addr data = app_data;
-    
-    new (SYSTEM) Task(as, cs, ds, main, code, data);
+    Task::self()->address_space()->detach(cs);
+    Task::self()->address_space()->detach(ds);
 
+
+    /*
+        Depois disso fazer detach da task corrente 
+            e então fazer attach no address space da task nova. 
+            
+            // TODO
+        Para os dados lembre-se de iterar o endereço de acordo 
+            com o tamanho dos segmentos para não haver sobreposição na carga.
+    */
+
+
+    // new (SYSTEM) Task(as, cs, ds, main, code, data);
+    new (SYSTEM) Task(cs, ds, main, code, data);
 
 
     db<Task>(ERR) << "************End::Load_app:************" << endl;
     return (*reinterpret_cast<int*> (addr + off_set + tamanho + 4));
+
+    /*
+        app_loader <-- argc argv que sao os elf tudo.
+            |
+            |-- app 1 (pode ter argc argv ou nao)
+            |
+            |-- app 2 ()
+
+            // extra_segment
+     */
+
 }
 __END_UTIL
 
