259f0aabd970b81fac8f72943a4021af43d1522f
Gabriel Simonetto <GabrielSimonetto@users.noreply.github.com>
2021-09-27 03:29:59 -0300

    remove thrash

--- execute_bla.sh
+++
@@ -1,7 +0,0 @@
-rm img/app_loader* 
-rm img/hello* 
-rm img/loader.img
-make APPLICATION=hello
-make APPLICATION=app_loader
-./bin/eposmkbi . ./img/loader.img ./img/app_loader ./img/hello
-/usr/bin/arm-none-eabi-objcopy -O binary img/loader.img img/app_loader.bin
